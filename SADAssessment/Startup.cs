c4e9612758f4cd288a6a100868effe39245d8ce5
Alexey Zakharenko <Alexey.Zakharenko@gmail.com>
2020-10-24 21:13:00 +0300

    Minor changes

--- SADAssessment/Startup.cs
+++ SADAssessment/Startup.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 
 namespace SADAssessment
 {
@@ -49,7 +41,6 @@ namespace SADAssessment
             services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
